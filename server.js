e83be55be49e7aecc0a51f54c773f4c98ff9c94a
cmadrid48 <cmadrid48@gmail.com>
2023-03-14 18:25:31 -0700

    updateEmployeeManager(); complete

--- server.js
+++ server.js
@@ -67,3 +67,64 @@ function init() {
         }
     });
 }
+
+function updateEmployeeManager() {
+    const query = `SELECT first_name, last_name FROM employee;`;
+    db.query(query, (req, res) => {
+        const employees = data.map(
+            (item) => `${item.first_name} ${item.last_name}`
+        );
+        
+        inquirer
+        .prompt([
+            {
+            name: "employee",
+            type: "list",
+            message: "which employee would you like to update?",
+            choices: employees,
+            },
+            ])
+            .then((answer) => {
+                const userSelect = answer.employee.split(" ");
+                const firstName = userSelect[0];
+                const lastName = userSelect[1];
+
+                const query = `SELECT 
+                first_name, last_name 
+                FROM employee 
+                WHERE manager_id IS NULL 
+                AND first_name != '${firstName}' 
+                AND last_name != '${lastName}';`;
+                db.query(query, (req, res) => {
+                const managers = data.map((item) => `${item.first_name}${item.last_name}`);
+
+                inquirer
+                .prompt({
+                    name: "manager",
+                    type: "list",
+                    message: "Who is the employee's new manager?",
+                    choices: managers,
+                })
+                .then((answer) => {
+                    const query = `SELECT id FROM employee WHERE first_name = ? AND last_name = ?`;
+                    db.query(query, [answer.manager.split(" ")[0], answer.manager.split(" ")[1]], (err, data) => {
+                        if (err) throw err;
+                        const managerId = data[0].id;
+                        
+                        const query = `UPDATE employee SET manager_id = ? WHERE first_name = ? AND last_name = ?`;
+                        db.query(
+                            query,
+                            [managerId, firstName, lastName],
+                            (err, data) => {
+                                if (err) throw err;
+                                console.log(`Successfully updated ${firstName} ${lastName}'s manager to ${answer.manager}.`);
+
+                                init();
+                            }
+                        );
+                    });
+                });
+            });
+        });
+    });
+}
